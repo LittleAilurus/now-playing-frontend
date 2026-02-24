6e0f5dc1846ad2ff85d0148f6330955f736a94b9
Widdit <hykhyk193729>
2026-02-24 20:39:42 +0800

    优化交互体验

--- src/pages/SettingsGeneral.tsx
+++ src/pages/SettingsGeneral.tsx
@@ -1146,7 +1146,7 @@ export default function GeneralSettingsPage() {
                     onPress={async () => {
                       setIsDetecting(true);
 
-                      await delay(1000);
+                      await delay(500);
 
                       try {
                         const response = await fetch(
